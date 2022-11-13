748e6820a6526a6328bbd737f74068f8a2bcb8a3
Колдун Ебучий <andrey11551611@gmail.com>
2022-11-13 13:55:17 +0300

    1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from get_image import Bing
+from song import SONG
+song= SONG('2XBYB2JH1TLMkmfWsoh5s9wKMPk3EZ_tsWdPHG487uYl9xPimmEh89Yk2SljBnTY')
 bing = Bing()
 print(bing.run('meme'))
--- song.py
+++ song.py
@@ -6,24 +6,26 @@ class SONG:
         self.song = name
         self.token= token
 
-genius = lyricsgenius.Genius('2XBYB2JH1TLMkmfWsoh5s9wKMPk3EZ_tsWdPHG487uYl9xPimmEh89Yk2SljBnTY')
-artist = genius.search_artist("Andy Shauf", max_songs=1, sort="title")
-song = artist.song("To You")
-temp=''
-n=0
-links=[]
-words=[]
-for i in song.lyrics.replace('\n'," "):
-    if i==" ":
-        if n<4:
-            temp=temp+i
-            n+=1
-        else:
-            words.append(temp)
-            temp=''
-            n=0
-    else:
-        temp = temp+i
-        n+=1
-    #print("DEBUG"+ " "+temp+" "+str(n))
-print(words)
+    def fing(self):
+        genius = lyricsgenius.Genius(self.token)
+        artist = genius.search_artist("Andy Shauf", max_songs=1, sort="title")
+        song = artist.song("To You")
+        temp = ''
+        n = 0
+        words = []
+        for i in song.lyrics.replace('\n', " "):
+            if i == " ":
+                if n < 4:
+                    temp = temp + i
+                    n += 1
+                else:
+                    words.append(temp)
+                    temp = ''
+                    n = 0
+            else:
+                temp = temp + i
+                n += 1
+            # print("DEBUG"+ " "+temp+" "+str(n))
+        print(words)
+
+
